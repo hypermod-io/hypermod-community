8235a179f5b7e759dc53b621d84b5394bd69e9d9
Daniel Del Core <daniel.delcore.92@gmail.com>
2021-05-27 11:54:48 +1000

    ✨ Adds menu codemods!

---
+++ community/@atlaskit__menu/0.7.0/_tests_/transform.spec.ts
@@ -0,0 +1,358 @@
+const defineInlineTest = require('jscodeshift/dist/testUtils').defineInlineTest;
+
+import * as transformer from '../transform';
+
+describe('@atlaskit/menu@0.7.0 transform', () => {
+  defineInlineTest(
+    { ...transformer, parser: 'tsx' },
+    {},
+    `
+      import { ButtonItem } from '@atlaskit/something';
+
+      const App = () => {
+        return <ButtonItem />;
+      }
+    `,
+    `
+      import { ButtonItem } from '@atlaskit/something';
+
+      const App = () => {
+        return <ButtonItem />;
+      }
+  `,
+    'leaves unrelated code untouched',
+  );
+
+  defineInlineTest(
+    { ...transformer, parser: 'tsx' },
+    {},
+    `
+      import { ButtonItem } from '@atlaskit/menu';
+
+      const App = () => {
+        return <ButtonItem cssFn={(styles, state) => ({
+
+        })} />;
+      }
+    `,
+    `
+      import { ButtonItem } from '@atlaskit/menu';
+
+      const App = () => {
+        return (
+          <ButtonItem cssFn={state => {
+
+          }} />
+        );
+      }
+  `,
+    'should remove current styles from inline function',
+  );
+
+  defineInlineTest(
+    { ...transformer, parser: 'tsx' },
+    {},
+    `
+      import { ButtonItem } from '@atlaskit/menu';
+
+      const App = () => {
+        return (
+          <div cssFn={(styles, state) => ({
+            ...styles,
+          })}>
+            <ButtonItem
+              cssFn={(styles, state) => ({
+                ...styles,
+              })}
+            />
+          </div>
+        );
+      };
+
+      `,
+    `
+      import { ButtonItem } from '@atlaskit/menu';
+
+      const App = () => {
+        return (
+          <div cssFn={(styles, state) => ({
+            ...styles,
+          })}>
+            <ButtonItem
+              cssFn={state => ({})}
+            />
+          </div>
+        );
+      };
+  `,
+    'should remove current styles from scope',
+  );
+
+  defineInlineTest(
+    { ...transformer, parser: 'tsx' },
+    {},
+    `
+    import { ButtonItem } from '@atlaskit/menu';
+
+    const App = () => {
+      return <ButtonItem cssFn={(styles, state) => ({
+        ...styles,
+        color: 'red'
+      })} />;
+    }
+      `,
+    `
+    import { ButtonItem } from '@atlaskit/menu';
+
+    const App = () => {
+      return (
+        <ButtonItem cssFn={state => ({
+          color: 'red'
+        })} />
+      );
+    }
+    `,
+    'correctly removes spread styles',
+  );
+
+  defineInlineTest(
+    { ...transformer, parser: 'tsx' },
+    {},
+    `
+    import { ButtonItem } from '@atlaskit/menu';
+
+    const App = () => {
+      return <ButtonItem cssFn={(styles, state) => ({
+        ...styles,
+        color: 'red',
+        ':hover': {
+          ...styles[':hover'],
+          color: 'blue'
+        }
+      })} />;
+    }
+      `,
+    `
+    import { ButtonItem } from '@atlaskit/menu';
+
+    const App = () => {
+      return (
+        <ButtonItem cssFn={state => ({
+          color: 'red',
+
+          ':hover': {
+            color: 'blue'
+          }
+        })} />
+      );
+    }
+    `,
+    'correctly removes spread styles with pseudo-selector access',
+  );
+
+  defineInlineTest(
+    { ...transformer, parser: 'tsx' },
+    {},
+    `
+    import { ButtonItem } from '@atlaskit/menu';
+
+    const App = () => {
+      return (
+        <ButtonItem cssFn={(styles, state) => ({
+          ...(state.x ? styles : {}),
+          color: 'red'
+        })} />
+      );
+    }
+      `,
+    `
+    import { ButtonItem } from '@atlaskit/menu';
+
+    const App = () => {
+      return (
+        <ButtonItem
+          /*
+          TODO: (@codeshift) The usage of the 'cssFn' prop in this component could not be transformed and requires manual intervention.
+          The 'cssFn' prop has been simplified so that users no longer need to merge the inherited styles with their own overrides.
+          For more info please reach out to #help-design-system-code.
+          */
+          cssFn={(styles, state) => ({
+            ...(state.x ? styles : {}),
+            color: 'red'
+          })} />
+      );
+    }
+      `,
+    'fail smoothly if the current styles are being used in a non-trivial way',
+  );
+
+  defineInlineTest(
+    { ...transformer, parser: 'tsx' },
+    {},
+    `
+    import { ButtonItem } from '@atlaskit/menu';
+
+    const App = () => {
+      return <ButtonItem cssFn={() => ({
+        color: 'red'
+      })} />;
+    }
+      `,
+    `
+    import { ButtonItem } from '@atlaskit/menu';
+
+    const App = () => {
+      return <ButtonItem cssFn={() => ({
+        color: 'red'
+      })} />;
+    }
+    `,
+    'not change anything if user is not using the current state or styles',
+  );
+
+  defineInlineTest(
+    { ...transformer, parser: 'tsx' },
+    {},
+    `
+    import { ButtonItem } from '@atlaskit/menu';
+
+    const App = () => {
+      return <ButtonItem cssFn={(styles, state) => ({
+        ...styles,
+        color: 'red'
+      })}>
+        <ButtonItem cssFn={(styles, state) => ({
+          ...styles,
+          color: 'red'
+        })}/>
+      </ButtonItem>;
+    }
+      `,
+    `
+    import { ButtonItem } from '@atlaskit/menu';
+
+    const App = () => {
+      return (
+        <ButtonItem cssFn={state => ({
+          color: 'red'
+        })}>
+          <ButtonItem cssFn={state => ({
+            color: 'red'
+          })}/>
+        </ButtonItem>
+      );
+    }
+    `,
+    'should remove styles in nested children',
+  );
+
+  defineInlineTest(
+    { ...transformer, parser: 'tsx' },
+    {},
+    `
+    import { ButtonItem } from '@atlaskit/menu';
+
+    const App = () => {
+      return <ButtonItem />;
+    }
+      `,
+    `
+    import { ButtonItem } from '@atlaskit/menu';
+
+    const App = () => {
+      return <ButtonItem />;
+    }
+    `,
+    'should leave affected items with no use of cssFn untouched',
+  );
+});
+
+describe('Updates and removes current styles', () => {
+  defineInlineTest(
+    { ...transformer, parser: 'tsx' },
+    {},
+    `
+    import { ButtonItem } from '@atlaskit/menu';
+
+    const cssFn = (styles, state) => ({
+      ...styles,
+      color: 'red'
+    });
+
+    const App = () => {
+      return <ButtonItem cssFn={cssFn} />;
+    }
+      `,
+    `
+    import { ButtonItem } from '@atlaskit/menu';
+
+    const cssFn = (styles, state) => ({
+      ...styles,
+      color: 'red'
+    });
+
+    const App = () => {
+      return (
+        <ButtonItem
+          /*
+          TODO: (@codeshift) The usage of the 'cssFn' prop in this component could not be transformed and requires manual intervention.
+          The 'cssFn' prop has been simplified so that users no longer need to merge the inherited styles with their own overrides.
+          For more info please reach out to #help-design-system-code.
+          */
+          cssFn={cssFn} />
+      );
+    }
+    `,
+    'should add prompt for non-inline function',
+  );
+
+  defineInlineTest(
+    { ...transformer, parser: 'tsx' },
+    {},
+    `
+    import { ButtonItem, LinkItem } from '@atlaskit/menu';
+
+    const cssFunction = (styles, state) => ({
+      ...styles,
+      color: 'red'
+    });
+
+    const App = () => {
+      return (
+        <ButtonItem cssFn={cssFunction}>
+          <LinkItem cssFn={cssFunction} />
+        </ButtonItem>
+      );
+    };
+      `,
+    `
+    import { ButtonItem, LinkItem } from '@atlaskit/menu';
+
+    const cssFunction = (styles, state) => ({
+      ...styles,
+      color: 'red'
+    });
+
+    const App = () => {
+      return (
+        <ButtonItem
+          /*
+          TODO: (@codeshift) The usage of the 'cssFn' prop in this component could not be transformed and requires manual intervention.
+          The 'cssFn' prop has been simplified so that users no longer need to merge the inherited styles with their own overrides.
+          For more info please reach out to #help-design-system-code.
+          */
+          cssFn={cssFunction}>
+          <LinkItem
+            /*
+            TODO: (@codeshift) The usage of the 'cssFn' prop in this component could not be transformed and requires manual intervention.
+            The 'cssFn' prop has been simplified so that users no longer need to merge the inherited styles with their own overrides.
+            For more info please reach out to #help-design-system-code.
+            */
+            cssFn={cssFunction} />
+        </ButtonItem>
+      );
+    };
+    `,
+    'should add prompt for non-inline function to multiple instances',
+  );
+});
---
+++ community/@atlaskit__menu/0.7.0/transform.ts
@@ -0,0 +1,131 @@
+import core, { API, FileInfo, Options } from 'jscodeshift';
+import { NodePath } from 'ast-types/lib/node-path';
+import {
+  hasImportDeclaration,
+  getJSXAttributesByName,
+  getDefaultImportSpecifierName,
+  getImportSpecifierName,
+} from '@codeshift/utils';
+
+function addCommentBeforeJSX(
+  j: core.JSCodeshift,
+  element: NodePath,
+  message: string,
+) {
+  function clean(value: string) {
+    return value
+      .replace(/[ \t]{2,}/g, ' ')
+      .replace(/^[ \t]*/gm, '')
+      .trim();
+  }
+
+  const content = `\nTODO: (@codeshift) ${clean(message)}\n`;
+  const comment = j.commentBlock(content, false, true);
+
+  element.insertBefore(comment);
+}
+
+function updateCssFnProp(
+  j: core.JSCodeshift,
+  source: ReturnType<typeof j>,
+  specifier: string,
+) {
+  source.findJSXElements(specifier).forEach(element => {
+    const cssFnPropCollection = getJSXAttributesByName(j, element, 'cssFn');
+
+    // no cssProp usage for this element
+    if (!cssFnPropCollection.length) return;
+
+    const cssFnProp = cssFnPropCollection.get();
+
+    const cssFnExpression = cssFnPropCollection
+      .find(j.JSXExpressionContainer)
+      .find(j.Expression)
+      .get();
+
+    if (cssFnExpression) {
+      // just remove the state styles param
+      try {
+        const [stylePropName] = cssFnExpression!.value.params;
+        j(cssFnExpression)
+          .find(j.SpreadElement)
+          .forEach(n => {
+            // discerns whether there are multiple identifiers here
+            const isComplexIdentifier = j(n).find(j.Identifier).length > 1;
+
+            if (isComplexIdentifier) {
+              throw new Error(
+                'CSSFn Prop codemod: Unable to parse spread element',
+              );
+            }
+
+            const hasStyleProp = !!j(n)
+              .find(j.Identifier)
+              .filter(node => node.value.name === stylePropName.name).length;
+            if (hasStyleProp) {
+              j(n).remove();
+            }
+          });
+
+        cssFnExpression!.value.params.shift();
+      } catch (e) {
+        addCommentBeforeJSX(
+          j,
+          cssFnProp,
+          `
+        The usage of the 'cssFn' prop in this component could not be transformed and requires manual intervention.
+        The 'cssFn' prop has been simplified so that users no longer need to merge the inherited styles with their own overrides.
+        For more info please reach out to #help-design-system-code.
+        `,
+        );
+      }
+    }
+  });
+}
+
+export default function transformer(
+  fileInfo: FileInfo,
+  { jscodeshift: j }: API,
+  options: Options,
+) {
+  const source = j(fileInfo.source);
+
+  if (!hasImportDeclaration(j, source, '@atlaskit/menu')) {
+    return fileInfo.source;
+  }
+
+  const defaultSpecifier = getDefaultImportSpecifierName(
+    j,
+    source,
+    '@atlaskit/menu',
+  );
+
+  if (defaultSpecifier != null) {
+    updateCssFnProp(j, source, defaultSpecifier);
+  }
+
+  [
+    'ButtonItem',
+    'LinkItem',
+    'CustomItem',
+    'HeadingItem',
+    'SkeletonItem',
+    'SkeletonHeadingItem',
+    'MenuGroup',
+    'PopupMenuGroup',
+    'Section',
+  ].forEach(pkg => {
+    const importSpecifier = getImportSpecifierName(
+      j,
+      source,
+      pkg,
+      '@atlaskit/menu',
+    );
+
+    if (importSpecifier != null) {
+      updateCssFnProp(j, source, importSpecifier);
+    }
+  });
+
+  return source.toSource(options.printOptions);
+}
