0e50a66a9c8b1afa9aa70954baa8671fa870d9be
Alex Reardon <areardon@atlassian.com>
2021-10-25 10:20:44 +1100

    failing tests for wrapping

--- community/memoize-one/src/5.0.0/transform.spec.ts
+++ community/memoize-one/src/5.0.0/transform.spec.ts
@@ -34,4 +34,86 @@ describe('memoize-one@5.0.0 transform', () => {
       `),
     );
   });
+
+  it('should wrap inline equality functions', () => {
+    const result = applyTransform(
+      transformer,
+      format(`
+        import memoize from 'memoize-one';
+
+        function add(a: number, b: number) {
+          return a + b;
+        }
+
+        const memoized = memoize(add, function isEqual(a, b) {
+          return a === b;
+        });
+      `),
+      { parser: 'tsx' },
+    );
+
+    expect(result).toEqual(
+      format(`
+      import memoize from 'memoize-one';
+
+      function add(a: number, b: number) {
+        return a + b;
+      }
+
+      const memoized = memoize(add, (newArgs, lastArgs) => {
+        if (newArgs.length !== lastArgs.length) {
+          return false;
+        }
+
+        function isEqual(a, b) {
+          return a === b;
+        }
+
+        return newArgs.every((newArg, index) => isEqual(newArg, lastArgs[index]));
+      });
+      `),
+    );
+  });
+
+  it('should wrap references', () => {
+    const result = applyTransform(
+      transformer,
+      format(`
+        import memoize from 'memoize-one';
+
+        function isEqual(a, b) {
+          return a === b;
+        }
+
+        function add(a: number, b: number) {
+          return a + b;
+        }
+
+        const memoized = memoize(add, isEqual);
+      `),
+      { parser: 'tsx' },
+    );
+
+    expect(result).toEqual(
+      format(`
+        import memoize from "memoize-one";
+
+        function isEqual(a, b) {
+          return a === b;
+        }
+
+        function add(a: number, b: number) {
+          return a + b;
+        }
+
+        const memoized = memoize(add, (newArgs, lastArgs) => {
+          if (newArgs.length !== lastArgs.length) {
+            return false;
+          }
+
+          return newArgs.every((newArg, index) => isEqual(newArg, lastArgs[index]));
+        });
+      `),
+    );
+  });
 });
