97239163c95e0079ce7b077065616941bcb17b31
Daniel Del Core <daniel.delcore.92@gmail.com>
2023-02-11 14:36:08 +1100

    updates test template to use approprate async

--- packages/initializer/template/codemods/codemod/transform.spec.ts
+++ packages/initializer/template/codemods/codemod/transform.spec.ts
@@ -2,8 +2,8 @@ import { applyTransform } from '@codeshift/test-utils';
 import * as transformer from './transform';
 
 describe('<% packageName %><% seperator %><% transform %> transform', () => {
-  it('should transform correctly', () => {
-    const result = applyTransform(
+  it('should transform correctly', async () => {
+    const result = await applyTransform(
       transformer,
       `
         import foo from '<% packageName %>';
