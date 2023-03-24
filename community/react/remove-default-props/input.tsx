67d5d36e2badaf18676d0da0895e6790a5c07c6e
Pavlo-Bohdan Ravliv <ravliv.pavlobohdan@gmail.com>
2023-03-24 09:35:11 +0100

    remove unused files

--- community/react/remove-default-props/input.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export function Component() {
-  return <span>Hi {name}</span>;
-}
-
-Component.defaultProps = { name: 'Oleg' };
--- community/react/remove-default-props/input2.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const Component = () => {
-  return <span>Hi {name}</span>;
-};
-
-Component.defaultProps = { name: 'Oleg' };
