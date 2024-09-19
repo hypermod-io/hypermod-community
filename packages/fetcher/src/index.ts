ee13161dd60407dbe8560f498be84dbad100753d
Daniel Del Core <ddelcore@atlassian.com>
2024-09-19 14:17:54 +1000

    Attempt to use tsx

--- packages/fetcher/src/index.ts
+++ packages/fetcher/src/index.ts
@@ -5,39 +5,12 @@ import globby from 'globby';
 import { PluginManager } from 'live-plugin-manager';
 
 import { Config } from '@hypermod/types';
+import { register } from 'tsx/esm/api';
 
-// This configuration allows us to require TypeScript config files directly
-const { DEFAULT_EXTENSIONS } = require('@babel/core');
-const presets = [];
-
-let presetEnv;
-try {
-  presetEnv = require('@babel/preset-env');
-  presets.push([presetEnv.default, { targets: { node: true } }]);
-} catch (_) {}
-
-require('@babel/register')({
-  configFile: false,
-  babelrc: false,
-  presets: [...presets, require('@babel/preset-typescript').default],
-  plugins: [
-    require('@babel/plugin-transform-class-properties').default,
-    require('@babel/plugin-transform-nullish-coalescing-operator').default,
-    require('@babel/plugin-transform-optional-chaining').default,
-    require('@babel/plugin-transform-modules-commonjs').default,
-    require('@babel/plugin-transform-private-methods').default,
-  ],
-  extensions: [...DEFAULT_EXTENSIONS, '.ts', '.tsx'],
-  // By default, babel register only compiles things inside the current working directory.
-  // https://github.com/babel/babel/blob/2a4f16236656178e84b05b8915aab9261c55782c/packages/babel-register/src/node.js#L140-L157
-  ignore: [
-    // Ignore parser related files
-    /@babel\/parser/,
-    /\/flow-parser\//,
-    /\/recast\//,
-    /\/ast-types\//,
-  ],
-});
+/**
+ * Register the TSX plugin to allow require TS(X) files.
+ */
+register();
 
 export interface ConfigMeta {
   filePath: string;
