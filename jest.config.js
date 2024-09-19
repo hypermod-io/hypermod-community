9ddaf589cdc9c518808b8703cd4e404acb8a670d
Daniel Del Core <ddelcore@atlassian.com>
2024-09-19 10:46:41 +1000

    Revert "updates ts-jest vers"
    This reverts commit 52fe62a15aa20cfeba5062d5fb7272fa4d94faaa.

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,15 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   transform: {
-    '\\.[jt]sx?$': [
+    '^.+\\.ts$': [
       'ts-jest',
       {
-        diagnostics: { ignoreCodes: [1343] },
-        astTransformers: {
-          before: [
-            {
-              path: 'node_modules/ts-jest-mock-import-meta', // or, alternatively, 'ts-jest-mock-import-meta' directly, without node_modules.
-              options: {
-                metaObjectReplacement: { url: 'https://www.url.com' },
-              },
-            },
-          ],
-        },
+        tsconfig: 'tsconfig.test.json',
+        useESM: true,
       },
     ],
   },
+  extensionsToTreatAsEsm: ['.ts'],
   moduleFileExtensions: ['ts', 'js'],
   testRegex: '^.+\\.(spec|test)\\.(ts|js)$',
   snapshotSerializers: ['jest-serializer-html-string'],
@@ -26,9 +18,7 @@ module.exports = {
     'jest-watch-typeahead/testname',
   ],
   moduleNameMapper: {
-    // '@hypermod/(.*)\\.js': '<rootDir>/packages/$1/src',
-    '(.+)\\.js$': '$1',
-
+    '^(\\.{1,2}/.*)\\.js$': '$1',
     '@hypermod/(.*)$': '<rootDir>/packages/$1/src',
     '@codeshift/(.*)$': '<rootDir>/packages/$1/src',
   },
--- packages/cli/src/main.ts
+++ packages/cli/src/main.ts
@@ -19,6 +19,8 @@ import {
 } from './utils/file-system.js';
 import { getConfigPrompt, getMultiConfigPrompt } from './prompt.js';
 
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
 const ExperimentalModuleLoader = () => {
   const getInfo = (packageName: string) => {
     const entryPath = require.resolve(packageName);
